fce2a6aef10b6e04471fde0514ce5469e47e14d0
Erick <rick.mutua12@gmail.com>
2017-12-02 09:28:44 +0300

    created model class RiderProfile to hold info of a rider

--- rider/models.py
+++ rider/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class RiderProfile (models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profpic = models.ImageField(upload_to='profpic/', blank=True, default=False)
+
+    mobile = models.IntegerField()
+
+    def __str__(self):
+
+        return self.user.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+
+        RiderProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+
+    instance.profile.save()
