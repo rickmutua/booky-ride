7efc93ca9a40239c4746c29bd5a65ccf55e87be7
Erick <rick.mutua12@gmail.com>
2017-12-02 09:30:14 +0300

    added funsquare api to the application to help get current location of users

--- booky/settings.py
+++ booky/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+CLIENT_ID = config('CLIENT_ID')
+CLIENT_SECRET = config('CLIENT_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
