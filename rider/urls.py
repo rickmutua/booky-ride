fa7e7e664d7a0facd36a3cc108ae3b2a4c927365
Erick <rick.mutua12@gmail.com>
2017-12-05 09:12:17 +0300

    added media file settings

--- rider/urls.py
+++ rider/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,12 +11,21 @@ urlpatterns = [
 
     url(r'^$', views.rider, name='index-rider'),
 
-    url(r'^profile/(?P<username>[-_\w.]+)$', views.profile, name='profile'),
+    url(r'^update-profile/$', views.update_profile_rider, name='update-profile-rider'),
 
-    url(r'^update-profile/$', views.update_profile, name='update-profile'),
+    url(r'^travel/$', views.travel, name='travel'),
+
+    url(r'^update-travel-details/$', views.update_travel_details, name='update-travel-details'),
+
+    url(r'^driver-list/$', views.driver_list, name='driver-list'),
 
     url(r'^logout/$', logout, {'index': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 
     url(r'^accounts/', include('registration.backends.simple.urls')),
 
 ]
+
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
