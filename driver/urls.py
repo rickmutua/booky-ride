9b3b254e5091348602a722a8ed83178287d4861a
Erick <rick.mutua12@gmail.com>
2017-12-05 09:17:07 +0300

    added media url settings

--- driver/urls.py
+++ driver/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.conf import settings
 
 from . import views
@@ -14,11 +15,19 @@ urlpatterns = [
 
     url(r'landing/^$', TemplateView.as_view(template_name='landing.html')),
 
-    url(r'^profile/(?P<username>[-_\w.]+)$', views.profile, name='profile'),
+    url(r'^update-profile/$', views.update_profile_driver, name='update-profile-driver'),
 
-    url(r'^update-profile/$', views.update_profile, name='update-profile'),
+    url(r'^vehicle/$', views.vehicle, name='vehicle'),
 
+    url(r'^update-vehicle-details$', views.update_vehicle_details, name='update-vehicle-details'),
+
+    url(r'^location/$', views.driver_location, name='driver-location'),
 
     url(r'^logout/$', logout, {'index': settings.LOGOUT_REDIRECT_URL}, name='logout')
 
 ]
+
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
