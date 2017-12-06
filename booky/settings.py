3bdde0d9ad28c586fde7efd98d3bf3adae7540fb
Erick <rick.mutua12@gmail.com>
2017-12-06 09:44:31 +0300

    deployment to heroku

--- booky/settings.py
+++ booky/settings.py
@@ -47,7 +47,7 @@ GEOPOSITION_MARKER_OPTIONS = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://booky-ride.herokuapp.com/', '127.0.0.1']
 
